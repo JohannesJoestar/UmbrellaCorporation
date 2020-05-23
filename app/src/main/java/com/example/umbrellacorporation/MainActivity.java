e105095cf62c28f769e0937f11662403e0902896
Johannes Joestar <thejohannesjoestar@gmail.com>
2020-05-24 01:18:03 +0300

    implemented push notification subscribtion

--- app/src/main/java/com/example/umbrellacorporation/MainActivity.java
+++ app/src/main/java/com/example/umbrellacorporation/MainActivity.java
@@ -1,14 +1,62 @@
 package com.example.umbrellacorporation;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 public class MainActivity extends AppCompatActivity {
 
+    //// Properties
+    // Components
+    Button btnHistory, btnVision, btnAddress, btnSupport;
+    // References
+    String token;
+
+    //// Methods
+    // AppCompatActivity implementation
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Test Firebase connection and obtain device token
+        FirebaseInstanceId.getInstance().getInstanceId()
+                .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                        if (task.isSuccessful()){
+                            token = task.getResult().getToken();
+                        }
+                    }
+                });
+
+        // Subscribe to push notifications
+        FirebaseMessaging.getInstance().subscribeToTopic(
+                getResources().getString(R.string.notifications_channel)
+        ).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+
+                    // Inform user of subscription state
+                    Toast.makeText(
+                            getApplicationContext(),
+                            (task.isSuccessful() ?
+                                    "Successfully subscribed to notifications." :
+                                    "Unable to subscribe to notifications."),
+                            Toast.LENGTH_LONG
+                    ).show();
+                }
+            });
     }
 }
