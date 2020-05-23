9f70ffc3a4ea7c2cddc425b450245e933f29f91d
Johannes Joestar <thejohannesjoestar@gmail.com>
2020-05-24 01:38:13 +0300

    logging payload

--- app/src/main/java/com/example/umbrellacorporation/service/CloudMessagingService.java
+++ app/src/main/java/com/example/umbrellacorporation/service/CloudMessagingService.java
@@ -1,15 +1,34 @@
 package com.example.umbrellacorporation.service;
 
+import android.util.Log;
+
+import com.example.umbrellacorporation.R;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+import java.util.Iterator;
 import java.util.Map;
 
 public class CloudMessagingService extends FirebaseMessagingService {
 
     //// Methods
     // FirebaseMessagingService Implementation
+    @Override
     public void onMessageReceived(RemoteMessage message){
-        Map<String, String> payload = message.getData();
+        logPayload(message.getData());
+    }
+    // Utility
+    private void logPayload(Map<String, String> payload){
+        Iterator<String> iterator = payload.keySet().iterator();
+
+        // Iterate message payload
+        while (iterator.hasNext()){
+            String key = iterator.next();
+            String value = payload.get(key);
+
+            Log.d("MainActivity", String.format(
+                    getResources().getString(R.string.payload_log_format), key, value)
+            );
+        }
     }
 }
