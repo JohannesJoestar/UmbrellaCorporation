91d6aa069623c3dbc35907c2549b2c539862cf59
Johannes Joestar <thejohannesjoestar@gmail.com>
2020-05-24 03:02:41 +0300

    broadcast receiver and alert dialog

--- app/src/main/java/com/example/umbrellacorporation/MainActivity.java
+++ app/src/main/java/com/example/umbrellacorporation/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.umbrellacorporation;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.example.umbrellacorporation.service.CloudMessagingService;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
@@ -23,14 +29,31 @@ public class MainActivity extends AppCompatActivity {
     Button btnHistory, btnVision, btnAddress, btnSupport;
     // References
     Resources resources = getResources();
+    IntentFilter IF;
+    BroadcastReceiver BR;
     String token;
 
     //// Methods
-    // AppCompatActivity implementation
+    // Activity life cycle
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Setup BroadcastReceiver for toasting Firebase notifications
+        BR = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                Bundle extras = intent.getExtras();
+
+                // Alerts more suitable than Toasts for notifications
+                AlertDialog.Builder ADB = new AlertDialog.Builder(context);
+                ADB.setTitle(extras.getString(CloudMessagingService.I_EXTRA_NOTIFICATION_TITLE));
+                ADB.setTitle(extras.getString(CloudMessagingService.I_EXTRA_NOTIFICATION_BODY));
+                ADB.setNegativeButton(R.string.adb_ok, null);
+            }
+        };
+        IF = new IntentFilter(CloudMessagingService.BR_FILTER_ACTION);
 
         // Test Firebase connection and obtain device token
         FirebaseInstanceId.getInstance().getInstanceId()
@@ -60,5 +83,21 @@ public class MainActivity extends AppCompatActivity {
                     ).show();
                 }
             });
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Registering BroadcastReceiver
+        if (BR != null) registerReceiver(BR, IF);
+
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+
+        // Unregistering BroadcastReceiver
+        if (BR != null) unregisterReceiver(BR);
+
     }
 }
